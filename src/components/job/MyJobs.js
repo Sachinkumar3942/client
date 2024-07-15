756ee4ee0955b3db39f96367e0160c51976a3064
Sachin kumar <122202436+Sachinkumar3942@users.noreply.github.com>
2024-07-15 21:43:06 +0530

    Update MyJobs.js

--- src/components/job/MyJobs.js
+++ src/components/job/MyJobs.js
@@ -15,7 +15,7 @@ const MyJobs = () => {
   useEffect(()=>{
     const handleCheck=async ()=>{
       try{
-        const response=await axios.get("http://localhost:5600/api/v1/user/getuser",{withCredentials: true});
+        const response=await axios.get("https://job-server-9p16.onrender.com/user/getuser",{withCredentials: true});
         console.log(check);
         if(response.data.user){
           setCheck(response.data.user);
