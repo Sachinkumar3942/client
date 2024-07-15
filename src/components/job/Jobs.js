e51e4debd3e53836eb64fdb3f8ee9f7304783e08
Sachin kumar <122202436+Sachinkumar3942@users.noreply.github.com>
2024-07-15 21:42:43 +0530

    Update Jobs.js

--- src/components/job/Jobs.js
+++ src/components/job/Jobs.js
@@ -11,7 +11,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:5600/api/v1/job/getall", {
+        .get("https://job-server-9p16.onrender.com/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
